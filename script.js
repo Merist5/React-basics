875f412ea73875e83d32e95aeaca79532b69feb2
Merist5 <37301295+Merist5@users.noreply.github.com>
2018-06-06 01:05:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ var movies = [
 
 var moviesElements = movies.map(function(movie) {
   return React.createElement('li', {key: movie.id},
-    React.createElement('img', {src: movie.poster}),
+    React.createElement('img', {src: movie.poster, alt: movie.title}),
       React.createElement('h2', {}, movie.title),
       React.createElement('p', {}, movie.desc)
     );
